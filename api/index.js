7c167cbbc6abb0d3a07f40de769739b59a774f4f
Baha Tarlaci <bahaanil.tarlaci@medyasoft.com.tr>
2023-05-22 22:55:37 +0300

    feat: Checking if logged in and logout functionality

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bycrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bycrypt.genSaltSync(10);
 const secret = 'mysecretsshhh';
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://root:sETfYCGdInn7Qll9@mernblog.1sxxbjv.mongodb.net/?retryWrites=true&w=majority');
 
@@ -33,10 +35,22 @@ app.post('/login', async (req, res) => {
     const userDoc = await User.findOne({username});
     const passOk = bycrypt.compareSync(password, userDoc.password);
     if (passOk) {
-        jwt.sign({username,id:userDoc.id}, secret, {expiresIn: '1h'}, (err, token) => {res.cookie('token', token).json('Logged In')});
+        jwt.sign({username,id:userDoc.id}, secret, (err, token) => {res.cookie('token', token).json('Logged In')});
     } else {
         res.status(401).json({message: 'Invalid Credentials'});
     }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) throw err;
+        res.json(decoded);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('token').json({message: 'Logged Out'});
 });
 
 app.listen(4000);
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include"
+    }).then((res) => {
+      res.json().then((data) => {
+        setUsername(data.username);
+      }
+    )});
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: 'POST'
+    }).then((res) => {
+      res.json().then((data) => {
+        setUsername(null);
+      }
+    )});
+  }
+
     return (
         <header>
         <Link to='/' className='logo'>My MernBlog</Link>
         <nav>
-          <Link to='/login'>Login</Link>
-          <Link to='/register'>Register</Link>
+          {username && (
+            <>
+              <Link to='/create'>Create Post</Link>
+              <Link to='/profile'>Profile</Link>
+              <Link onClick={logout}>Logout</Link>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to='/login'>Login</Link>
+              <Link to='/register'>Register</Link>
+            </>
+          )}
         </nav>
       </header>
     )
