d53115626595ff2e06d1a8bf74bb4ace9d1bcffc
Baha Tarlaci <bahaanil.tarlaci@medyasoft.com.tr>
2023-05-22 18:42:19 +0300

    feat: Client setup has been completed until the Authentication part, pages have been created.

---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import Post from "../components/Posts/Post";
+
+export default function IndexPage() {
+    return (
+        <>
+            <Post />
+            <Post />
+            <Post />
+        </>
+    )
+}
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <form className="login">
+                <h1>Login</h1>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
